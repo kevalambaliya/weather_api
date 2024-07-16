05e2ea22b546fdc4f8c8156296e4c6cb9f305ae2
kevalambaliya <148741207+kevalambaliya@users.noreply.github.com>
2024-07-16 15:15:36 +0530

    Add files via upload

---
+++ weather api/script.js
@@ -0,0 +1,20 @@
+document.getElementById("btn").addEventListener("click", function (event){
+    event.preventDefault();
+
+    let city = document.getElementById("inp-city").value;
+    let api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=217d9466fcece181f108ef7574f237b4`;
+
+    fetch(api)
+    .then((res) => res.json())
+    .then((data) => {
+        document.getElementById("res").innerHTML = data.weather[0].main;
+
+        let icon = document.getElementById("icon");
+
+        
+      let iconurl =
+      "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+
+      icon.src = iconurl;
+    });
+});
